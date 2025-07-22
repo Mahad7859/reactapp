09b931b653abca43c2b86f506615d9ca5e0c9594
vboxuser <vboxuser@Ubuntu2.myguest.virtualbox.org>
2025-07-22 14:36:36 +0500

    Fixed the Duplication error

--- main.tf
+++ main.tf
@@ -13,19 +13,6 @@ terraform {
     }
   }
 }
-
-provider "google" {
-  credentials = jsondecode(var.google_credentials)
-  project     = "your-gcp-project-id"     # Replace this
-  region      = "us-central1"             # Replace as needed
-}
-
-variable "google_credentials" {
-  description = "GCP credentials"
-  type        = string
-  sensitive   = true
-}
-
 provider "google" {
   credentials = jsondecode(var.google_credentials)
   project     = var.project_id
--- variables.tf
+++ variables.tf
@@ -1,8 +1,3 @@
-variable "project_id" {
-  type        = string
-  description = "The GCP project ID"
-}
-
 variable "region" {
   type        = string
   default     = "us-central1"
