fc403eb205a3dc695bce71701f190765e40afdcc
vboxuser <vboxuser@Ubuntu2.myguest.virtualbox.org>
2025-07-22 12:47:27 +0500

    Added Variables files

--- main.tf
+++ main.tf
@@ -7,3 +7,32 @@ terraform {
     }
   }
 }
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 5.0"
+    }
+  }
+
+  cloud {
+    organization = "ASLII"              # ✅ Your Terraform Cloud Org
+    workspaces {
+      name = "reactapp"                 # ✅ Your Workspace Name
+    }
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_storage_bucket" "static_bucket" {
+  name     = "${var.project_id}-static-site-bucket"
+  location = var.region
+  force_destroy = true
+
+  uniform_bucket_level_access = true
+}
+
